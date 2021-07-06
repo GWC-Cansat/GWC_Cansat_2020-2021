89b4884e3aae2cca96600cc5b6342637eb55cf06
Fraser Price <fraserbcprice@gmail.com>
2021-07-06 22:14:07 +0100

    Latest version with fault tolerance

--- code/cansat.ino
+++ code/cansat.ino
@@ -4,178 +4,212 @@
 #include <Adafruit_GPS.h>
 #include <Wire.h>
 #include <Serial.h>
+#include <time.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME680.h>
 
-#define BME_SCK 9
-#define BME_MISO 11
-#define BME_MOSI 12
-#define BME_CS 10
+#define SEALEVELPRESSURE_HPA 1013.25
+#define SAMPLE_RATE          50
 
-#define SEALEVELPRESSURE_HPA (1013.25)
+// BME680 GAS sensor [CS:10, MOSI:12, MISO:11, SCK:9]
+Adafruit_BME680 bme(10, 12, 11,	9);
 
-Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK);
+// HC12 radio module [TX:5 RX:4]
+UART HC12(digitalPinToPinName(4), digitalPinToPinName(5), NC, NC);
 
-// Connect the GPS Power pin to 5V
-// Connect the GPS Ground pin to ground
-// Connect the GPS TX (transmit) pin to Digital 3
-// Connect the GPS RX (receive) pin to Digital 2
-// You will also have to edit "pings_arduino.txt" which can be found in the variants folder
-// Change
-// #define PIN_SERIAL_RX (1ul)
-// #define PIN_SERIAL_TX (0ul)
-// To
-// #define PIN_SERIAL_RX (3ul)
-// #define PIN_SERIAL_TX (2ul)
+/**
+ * Connect the GPS Power pin to 5V
+ * Connect the GPS Ground pin to ground
+ * Connect the GPS TX (transmit) pin to Digital 3
+ * Connect the GPS RX (receive) pin to Digital 2
+ * You will also have to edit "pings_arduino.txt" which can be found in the variants folder
+ * Change
+ * #define PIN_SERIAL_RX (1ul)
+ * #define PIN_SERIAL_TX (0ul)
+ * To
+ * #define PIN_SERIAL_RX (3ul)
+ * #define PIN_SERIAL_TX (2ul)
+ */
 
 // you can change the pin numbers to match your wiring:
 Adafruit_GPS GPS(&_UART1_);
 
-//HC12 radio module [TX:5 RX:4]
-UART HC12(digitalPinToPinName(4), digitalPinToPinName(5), NC, NC);
+/**
+ * Status of the onboard sensors
+ * 7     5      4     3     2     1     0
+ * |-----| BARO | HTS | IMU | GPS | BME |
+ */
 
-
-// Define this
-int timeToWait;
-#define LED 13 //on-board led
+unsigned char DeviceStatus = 0;
 
 void setup() {
-  //_UART1_.begin(9600);
-  HC12.begin(9600);
+	// Start the serial connection to the HC12
+	HC12.begin(9600);
 
-  // Start the pressure sensor
-  if (!BARO.begin()) {
-    //Serial.println("Failed to initialize pressure sensor!");
-    while (1);
-  }
+	// Reset the status
+	DeviceStatus.status = 0;
 
-  // Start the humidity and temperature sensor
-  if (!HTS.begin()) {
-    //Serial.println("Failed to initialize humidity temperature sensor!");
-    while (1);
-  }
+	// Start the barometer
+	if (!BARO.begin()) {
+		DeviceStatus |= 0x10;
+	}
 
-  // Start the IMU
-  if (!IMU.begin()) {
-    //Serial.println("Failed to initialize IMU!");
-    while (1);
-  }
+	// Start the humidity and temperature sensor
+	if (!HTS.begin()) {
+		DeviceStatus |= 0x08;
+	}
 
-  // Get the speed of all the IMU sensors so we determine how long to wait
-  int AccelSampleRate = IMU.accelerationSampleRate();
-  int GyroSampleRate = IMU.gyroscopeSampleRate();
-  int MagnetSampleRate = IMU.magneticFieldSampleRate();
+	// Start the IMU
+	if (!IMU.begin()) {
+		DeviceStatus |= 0x04;
+	}
 
-  // Get the time to wait in milliseconds by (1 / LowestSampleRate)*1000
-  int timeToWait = (1 / min(AccelSampleRate, min(GyroSampleRate, MagnetSampleRate))) * 1000;
-
-  // Configure the GPS
-  GPS.begin(9600);
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
-  GPS.sendCommand(PGCMD_ANTENNA);
-
-  if (!bme.begin()) {
-    //Serial.println("Could not find a valid BME680 sensor, check wiring!");
-    while (1);
-  }
-
-  // Set up oversampling and filter initialization
-  bme.setTemperatureOversampling(BME680_OS_8X);
-  bme.setHumidityOversampling(BME680_OS_2X);
-  bme.setPressureOversampling(BME680_OS_4X);
-  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
-  bme.setGasHeater(320, 150); // 320*C for 150 ms
-
-  //Serial.println("Started");
-
-  pinMode(LED, OUTPUT);
+	// Configure the GPS
+	if(!GPS.begin(9600)) {
+		DeviceStatus |= 0x02;
+	} else {
+		GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+		GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+		GPS.sendCommand(PGCMD_ANTENNA);
+	}
+	
+	// Configure the BME680
+	if (!bme.begin()) {
+		DeviceStatus |= 0x01;
+	} else {
+		// Set up oversampling and filter initialization
+		bme.setTemperatureOversampling(BME680_OS_8X);
+		bme.setHumidityOversampling(BME680_OS_2X);
+		bme.setPressureOversampling(BME680_OS_4X);
+		bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+		bme.setGasHeater(320, 150); // 320*C for 150 ms
+	}
 }
 
 void loop() {
-  
-  // Read the pressure, temperature and humidity from the onboard sensors
-  float onbPressure = BARO.readPressure();
-  float onbTemperature = HTS.readTemperature();
-  float onbHumidity = HTS.readHumidity();
+	// Send the device status
+	HC12.write(DeviceStatus);
 
-  // Read the BME680 data
-  float bmeTemperature = 0.0, bmePressure = 0.0, bmeHumidity = 0.0, gasResistance = 0.0, altitude = 0.0;
-  if (bme.performReading()) {
-    bmeTemperature = bme.temperature;
-    bmePressure = bme.pressure / 100.0;
-    bmeHumidity = bme.humidity;
-    gasResistance = bme.gas_resistance / 1000.0;
-    altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
-  }
-  
-  // Get the gyroscope values
-  float Gx, Gy, Gz;
-  while (!IMU.gyroscopeAvailable()) {}
-  IMU.readGyroscope(Gx, Gy, Gz);
+	// Barometer
+	if(!(DeviceStatus & 0x10)) {
+		// Sensor type
+		HC12.write(0x00);
 
-  // Get the magnetometer values
-  float Mx, My, Mz;
-  while (!IMU.magneticFieldAvailable()) {}
-  IMU.readMagneticField(Mx, My, Mz);
+		// Get the pressure
+		float pressure = BARO.readPressure();
 
-  // Get the accelerometer values
-  float Ax, Ay, Az;
-  while (!IMU.accelerationAvailable()) {}
-  IMU.readAcceleration(Ax, Ay, Az);
+		// Send the data
+		HC12.write(&pressure, sizeof(float));
+	}
 
-  // put the values into an array
-  double buf[] = {onbPressure, onbTemperature, onbHumidity, bmeTemperature, bmePressure, bmeHumidity, gasResistance, altitude, Ax, Ay, Az, Mx, My, Mz, Gx, Gy, Gz};
+	// Humidity and Temperature Sensor
+	if(!(DeviceStatus & 0x08)) {
+		// Sensor type
+		HC12.write(0x01);
 
-  // Send data from on-board sensors to HC12
-  for (int x = 0; x < sizeof(buf) / sizeof(double); x++) {
-    HC12.print(buf[x]);
-    HC12.print(",");
-  }
+		// Get the temperature and humidity
+		float temperature = HTS.readTemperature();
+		float humidity = HTS.readHumidity();
 
-  // Check for GPS data
-  int bytes = GPS.available();
-  for(int x = 0; x < bytes; x++) GPS.read();
-  if (GPS.newNMEAreceived()) {
-    // Decode GPS data
-    GPS.parse(GPS.lastNMEA());
-  }
-  
-  // GPS fix and quality data
-  HC12.print((int)GPS.fix);
-  HC12.print(",");
-  HC12.print((int)GPS.fixquality);
-  
-  // Time
-  HC12.print(GPS.hour, DEC); HC12.print(':');
-  HC12.print(GPS.minute, DEC); HC12.print(':');
-  HC12.print(GPS.seconds, DEC); HC12.print('.');
-  HC12.print(GPS.milliseconds);
-  
-  // Date
-  HC12.print(GPS.day, DEC); HC12.print('/');
-  HC12.print(GPS.month, DEC); HC12.print("/20");
-  HC12.print(GPS.year, DEC);
-  HC12.print(",");
+		// Send the data
+		HC12.write(&temperature, sizeof(float));
+		HC12.write(&humidity, sizeof(float));
+	}
 
-  // Location
-  HC12.print(GPS.latitude, 4);
-  HC12.print(GPS.lat);
-  HC12.print(",");
-  HC12.print(GPS.longitude, 4);
-  HC12.print(GPS.lon);
-  HC12.print(",");
+	// Inertial Measurement Unit
+	if(!(DeviceStatus & 0x04)) {
+		// Sensor type
+		HC12.write(0x02);
 
-  // Speed, Angle and Altitude
-  HC12.print(GPS.speed);
-  HC12.print(",");
-  HC12.print(GPS.angle);
-  HC12.print(",");
-  HC12.print(GPS.altitude);
-  HC12.print(",");
-  HC12.print((int)GPS.satellites);
+		// Get the gyroscope values
+		float Gx, Gy, Gz;
+		while (!IMU.gyroscopeAvailable()) {}
+		IMU.readGyroscope(Gx, Gy, Gz);
 
-  HC12.println();
-  delay(timeToWait);
+		// Get the magnetometer values
+		float Mx, My, Mz;
+		while (!IMU.magneticFieldAvailable()) {}
+		IMU.readMagneticField(Mx, My, Mz);
+
+		// Get the accelerometer values
+		float Ax, Ay, Az;
+		while (!IMU.accelerationAvailable()) {}
+		IMU.readAcceleration(Ax, Ay, Az);
+
+		// Send the data
+		HC12.write(&Gx, sizeof(float));
+		HC12.write(&Gy, sizeof(float));
+		HC12.write(&Gz, sizeof(float));
+		HC12.write(&Mx, sizeof(float));
+		HC12.write(&My, sizeof(float));
+		HC12.write(&Mz, sizeof(float));
+		HC12.write(&Ax, sizeof(float));
+		HC12.write(&Ay, sizeof(float));
+		HC12.write(&Az, sizeof(float));
+	}
+
+	// GPS
+	if(!(DeviceStatus & 0x02)) {
+		// Sensor type
+		HC12.write(0x03);
+
+		// Read data from the GPS if any is avaliable
+		int size = GPS.available();
+		for(int x = 0; x < size; x++) GPS.read();
+
+		// Parse the NMEA string from the GPS
+		if (GPS.newNMEAreceived()) {
+			GPS.parse(GPS.lastNMEA());
+		}
+
+		// Convert the time to unix timestamp
+		struct tm t;
+		unsigned long epoch;
+
+		t.tm_year = GPS.year + 100;
+		t.tm_mon = GPS.month - 1;
+		t.tm_mday = GPS.day;
+		t.tm_hour = GPS.hour;
+		t.tm_min = GPS.minute;
+		t.tm_sec = GPS.seconds;
+
+		epoch = mktime(&t);
+
+		// Send the data
+		HC12.write(GPS.fix);
+		HC12.write(GPS.fixquality);
+		HC12.write(GPS.satellites);
+		HC12.write(&epoch, sizeof(unsigned long));
+		HC12.write(&GPS.latitude, sizeof(nmea_float_t));
+		HC12.write(&GPS.longitude, sizeof(nmea_float_t));
+		HC12.write(&GPS.speed, sizeof(nmea_float_t));
+		HC12.write(&GPS.angle, sizeof(nmea_float_t));
+		HC12.write(&GPS.altitude, sizeof(nmea_float_t));
+	}
+
+	// BME680
+	if(!(DeviceStatus & 0x01)) {
+		if (bme.performReading()) {
+			// Sensor type
+			HC12.write(0x04);
+
+			// Read the data
+			float temperature = bme.temperature;
+			float pressure = bme.pressure / 100.0;
+			float humidity = bme.humidity;
+			float gasResistance = bme.gas_resistance / 1000.0;
+			float altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+
+			// Send the data
+			HC12.write(&temperature, sizeof(float));
+			HC12.write(&pressure, sizeof(float));
+			HC12.write(&humidity, sizeof(float));
+			HC12.write(&gasResistance, sizeof(float));
+			HC12.write(&altitude, sizeof(float));
+		}
+	}
+
+	HC12.println();
+	delay(SAMPLE_RATE);
 }
